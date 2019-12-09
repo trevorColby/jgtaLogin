9a7e1064f1a08700fbed1cdec2128d75dbea557c
trevorColby <trevor.john.colby@gmail.com>
2019-12-08 17:46:41 -0800

    login.js: removed extra

--- login.js
+++ login.js
@@ -1,8 +1,8 @@
 //Get needed elements and store so we only iterate the DOM once
 var englishButton = document.getElementById('englishButton');
 var mandarinButton = document.getElementById('mandarinButton');
-var guestButton = document.getElementById('guestButton');
-var adminButton = document.getElementById('adminButton');
+// var guestButton = document.getElementById('guestButton');
+// var adminButton = document.getElementById('adminButton');
 var curLang = document.getElementById('curLang');
 var curLoginType = document.getElementById('curLoginType');
 var curFlag = document.getElementById('curFlag');
@@ -50,53 +50,53 @@ englishButton.addEventListener('click',function() {
 	password.placeholder = 'Enter Password';
 	login.innerHTML = 'Login';
 	langSelectFunc();
-	if(curLoginType.innerHTML == '管理员') {
-		curLoginType.innerHTML == 'Admin';
+	if(curLoginType.innerHTML === '管理员') {
+		curLoginType.innerHTML = 'Admin';
 	} 
-	else if (curLoginType.innerHTML == '来宾') {
-		curLoginType.innerHTML = 'Guest';
-	} 
-	guestButton.innerHTML = 'Guest';
-	adminButton.innerHTML = 'Admin';
+	// else if (curLoginType.innerHTML === '来宾') {
+	// 	curLoginType.innerHTML = 'Guest';
+	// } 
+	// guestButton.innerHTML = 'Guest';
+	// adminButton.innerHTML = 'Admin';
 });
 
 mandarinButton.addEventListener('click',function() {
 	curFlag.src = '../images/chinaFlag.png';
-	curLang.innerHTML = '语言';
+	curLang.innerHTML = '中文';
 	username.placeholder = '用户名';
 	password.placeholder = '密码';
 	login.innerHTML = '登录';
 	langSelectFunc();
-	if(curLoginType.innerHTML == 'Admin') {
-		curLoginType.innerHTML == '管理员';
+	if(curLoginType.innerHTML === 'Admin') {
+		curLoginType.innerHTML = '管理员';
 	} 
-	else if (curLoginType.innerHTML == 'Guest') {
-		curLoginType.innerHTML = '来宾';
-	}
-	guestButton.innerHTML = '来宾';
-	adminButton.innerHTML = '管理员';
+	// else if (curLoginType.innerHTML === 'Guest') {
+	// 	curLoginType.innerHTML = '来宾';
+	// }
+	// guestButton.innerHTML = '来宾';
+	// adminButton.innerHTML = '管理员';
 	//will also need to change guest/admin to mandarin
 });
 
-guestButton.addEventListener('click',function() {
-	//need to check current language and adjust accordingly
-	if(curLoginType.innerHTML == 'Admin') {
-		curLoginType.innerHTML = 'Guest';
-	} 
-	else if (curLoginType.innerHTML == '管理员') {
-		curLoginType.innerHTML = '来宾';
-	} 
-	//need business logic to send to different php page
-});
+//guestButton.addEventListener('click',function() {
+//	//need to check current language and adjust accordingly
+//	if(curLoginType.innerHTML === 'Admin') {
+//		curLoginType.innerHTML = 'Guest';
+//	} 
+//	else if (curLoginType.innerHTML === '管理员') {
+//		curLoginType.innerHTML = '来宾';
+//	} 
+//	//need business logic to send to different php page
+//});
 
-adminButton.addEventListener('click',function() {
-	if(curLoginType.innerHTML == 'Guest') {
-		curLoginType.innerHTML = 'Admin';
-	} 
-	else if(curLoginType.innerHTML = '来宾') {
-		curLoginType.innerHTML = '管理员';
-	}
-});
+// adminButton.addEventListener('click',function() {
+// 	if(curLoginType.innerHTML === 'Guest') {
+// 		curLoginType.innerHTML = 'Admin';
+// 	} 
+// 	else if(curLoginType.innerHTML === '来宾') {
+// 		curLoginType.innerHTML = '管理员';
+// 	}
+// });
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
@@ -104,17 +104,17 @@ function langSelectFunc() {
   document.getElementById("langDropdown").classList.toggle("show");
 }
 
-function loginTypeFunc() {
-  document.getElementById("loginTypeDropdown").classList.toggle("show");
-}
+// function loginTypeFunc() {
+//   document.getElementById("loginTypeDropdown").classList.toggle("show");
+// }
 
 window.onclick = function(e) {
 	if (!e.target.matches('.dropbtn.loginType')) {
-		var loginTypeDropdown = document.getElementById("loginTypeDropdown");
+		// var loginTypeDropdown = document.getElementById("loginTypeDropdown");
 		var langDropdown = document.getElementById("langDropdown");
-		if (loginTypeDropdown.classList.contains('show')) {
-			loginTypeDropdown.classList.remove('show');
-		}
+		// if (loginTypeDropdown.classList.contains('show')) {
+		// 	loginTypeDropdown.classList.remove('show');
+		// }
 		if (!e.target.matches('.dropbtn')) {
 			if (langDropdown.classList.contains('show')) {
 				langDropdown.classList.remove('show');
